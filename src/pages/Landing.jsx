a32a358a8e304dd0b2a0ce7a7e9d06c0be25dac8
coreyjhurst11-hub <coreyjhurst11@gmail.com>
2026-06-15 10:55:37 -0400

    Landing.jsx

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+React, { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Mark from "../Mark.jsx";
 import DemoFilm from "../components/DemoFilm.jsx";
